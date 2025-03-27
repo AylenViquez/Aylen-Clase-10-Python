361520219427b35b4c5ae50d02642b5444dd6ee0
aviquezm27 <102369819+aviquezm27@users.noreply.github.com>
2025-03-26 20:51:32 -0600

    Projecto Aylen Viquez

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
+# Estudiante: Aylen Viquez Madrigal
+# Fecha de Inicio: 26-03-2025
 # Fecha de Entrega: [dd/mm/aaaa]
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+from analisis_datos import *
+
+lista_compras = generar_lista_compras()
+print(lista_compras)
+
+precios = [valor for producto,valor in lista_compras]
+
+print('La media arimetica de la compra es :', media(precios))
+print('La mediana de la compra es : ', mediana(precios))
